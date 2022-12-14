39b4d154e89aee534d1e7dc963080b6c6a21a842
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2022-12-14 09:50:15 +0100

    chore: install dompurify

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import DOMpurify from 'dompurify';
+
+import NotFound from '../NotFound';
 
 import './styles.scss';
 
-function Post({
-  id,
-  title,
-  category,
-  excerpt,
-}) {
+function SinglePost({ posts }) { // on recupère la liste des posts de Blog via les props
+  // on recupere le parametre postId,
+  // déclaré dans la props path de la route dans Blog
+  // <Route path="/post/:postId" element={<SinglePost />} />
+  const { postId } = useParams();
+
+  // on essaye de retrouver le post désigné par le paramètre dans l'url (attention le params est un string)
+  const postFound = posts.find((post) => post.id === Number(postId));
+
+  if (!postFound) {
+    // si on le trouve pas on retourne la page de 404
+    return <NotFound />;
+  }
+
   return (
-    <article className="post">
-      <Link to={`/post/${id}`}>
-        <h2 className="post-title">{title}</h2>
-        <div className="post-category">{category}</div>
-        <p className="post-excerpt">{excerpt}</p>
-      </Link>
+    <article className="single">
+      <h2 className="post-title">{postFound.title}</h2>
+      <div className="post-category">{postFound.category}</div>
+      <p
+        className="post-excerpt"
+        dangerouslySetInnerHTML={{ __html: DOMpurify.sanitize(postFound.excerpt) }}
+      />
     </article>
   );
 }
 
-Post.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
-  excerpt: PropTypes.string.isRequired,
+SinglePost.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    excerpt: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
-export default React.memo(Post);
+export default React.memo(SinglePost);
+
